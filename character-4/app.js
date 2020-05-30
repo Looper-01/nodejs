b93b6f5dcb0dca2f1ed4a8307cbbe962af66c4a4
Looper <lzy_ok@aliyun.com>
2020-05-30 12:20:18 +0800

    [update] 综合http请求

--- character-4/app.js
+++ character-4/app.js
@@ -1,25 +1,43 @@
 /*
- * @Description: 
+ * @Description:
  * @Author: Looper
  * @Date: 2020-05-29 22:53:05
  * @LastEditors: Looper
  * @LastEditTime: 2020-05-29 23:04:50
  * @FilePath: /nodejs/character-4/app.js
- * @Url: 
+ * @Url:
  */
 const http = require("http");
 const querystring = require("querystring");
 
 const server = http.createServer((req, res) => {
-  console.log("req.method:", req.method);
-
+  const method = req.method;
   const url = req.url;
-  console.log("req.url:", url);
+  const path = url.split("?")[0];
+  const query = querystring.parse(url.split("?")[1]);
+  res.setHeader("Content-Type", "application/json");
 
-  req.query = querystring.parse(url.split("?")[1]);
-  console.log("req.query", JSON.stringify(req.query));
+  const resData = {
+    method,
+    url,
+    path,
+    query
+  }
 
-  res.end(JSON.stringify(req.query));
+  if (method === "GET") {
+    res.end(JSON.stringify(resData));
+  }
+
+  if (method === "POST") {
+    let postData = "";
+    req.on("data", chunk => {
+      postData += chunk.toString();
+    })
+    req.on("end", () => {
+      resData.postData = postData;
+      res.end(JSON.stringify(resData))
+    })
+  }
 });
 
 server.listen(8000)
