b218b9ff9f6df129056840c8e2e243594b8d05f5
Looper <lzy_ok@aliyun.com>
2020-06-06 23:58:40 +0800

    [add] login check by cookie, set cookie with httpOnly/expires

--- blog-1/src/router/user.js
+++ blog-1/src/router/user.js
@@ -3,12 +3,20 @@
  * @Author: Looper
  * @Date: 2020-05-31 21:14:03
  * @LastEditors: Looper
- * @LastEditTime: 2020-06-03 23:48:35
+ * @LastEditTime: 2020-06-06 23:53:50
  * @FilePath: /nodejs/blog-1/src/router/user.js
  * @Url: 
  */
 const { loginCheck } = require("../controller/user")
 const { SuccessModel, ErrorModel } = require("../model/resModel")
+
+// 获取cookie的过期时间
+const getCookieExpires = () => {
+  const d = new Date();
+  d.setTime(d.getTime() + (24 * 60 * 60 * 1000));
+  console.log('d.toGMTString() is ', d.toGMTString());
+  return d.toGMTString();
+}
 
 const handleUserRouter = (req, res) => {
   const method = req.method;
@@ -19,6 +27,7 @@ const handleUserRouter = (req, res) => {
     const result = loginCheck(username, password);
     return result.then(data => {
       if (data.username) {
+        res.setHeader("Set-Cookie", `username=${data.username}; path=/; httpOnly; expires=${getCookieExpires()}`)
         return new SuccessModel("登录成功");
       }
       return new ErrorModel("登录失败");
