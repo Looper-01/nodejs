b5f9c7e9117893538a89b0578e5fdc6d59fdb4f8
Looper <lzy_ok@aliyun.com>
2020-06-07 12:05:28 +0800

    [update] 使用session 优化cookie认证

--- blog-1/app.js
+++ blog-1/app.js
@@ -3,13 +3,23 @@
  * @Author: Looper
  * @Date: 2020-05-31 21:14:03
  * @LastEditors: Looper
- * @LastEditTime: 2020-06-03 23:49:55
+ * @LastEditTime: 2020-06-07 11:39:24
  * @FilePath: /nodejs/blog-1/app.js
  */
 const querystring = require("querystring");
 const handleBlogRouter = require("./src/router/blog");
 const handleUserRouter = require("./src/router/user");
 
+// 设置cookie的过期时间
+const getCookieExpires = () => {
+  const d = new Date();
+  d.setTime(d.getTime() + (24 * 60 * 60 * 1000));
+  console.log('d.toGMTString() is ', d.toGMTString());
+  return d.toGMTString();
+}
+
+// session数据
+const SESSION_DATA = {};
 
 const getPostData = (req) => {
   return new Promise((resolve, reject) => {
@@ -45,6 +55,33 @@ const serverHandle = (req, res) => {
   // 解析query
   req.query = querystring.parse(url.split("?")[1])
 
+  // 解析cookie
+  req.cookie = {};
+  const cookieStr = req.headers.cookie || "";
+  cookieStr.split(";").forEach(item => {
+    if (!item) {
+      return
+    }
+    const arr = item.split("=");
+    const key = arr[0].trim();
+    const val = arr[1].trim();
+    req.cookie[key] = val;
+  });
+
+  // 解析session
+  let needSetCookie = false;
+  let userId = req.cookie.userid;
+  if (userId) {
+    if (!SESSION_DATA[userId]) {
+      SESSION_DATA[userId] = {}
+    }
+  } else {
+    needSetCookie = true;
+    userId = `${Date.now()}_${Math.random()}`
+    SESSION_DATA[userId] = {}
+  }
+  req.session = SESSION_DATA[userId];
+
   // 处理post data
   getPostData(req).then((postData) => {
     req.body = postData;
@@ -53,6 +90,9 @@ const serverHandle = (req, res) => {
     const blogResult = handleBlogRouter(req, res);
     if (blogResult) {
       blogResult.then(blogData => {
+        if (needSetCookie) {
+          res.setHeader("Set-Cookie", `userid=${userId};path=/; httpOnly;expires=${getCookieExpires()}`)
+        }
         res.end(JSON.stringify(blogData));
       })
       return;
@@ -62,6 +102,9 @@ const serverHandle = (req, res) => {
     const userResult = handleUserRouter(req, res);
     if (userResult) {
       userResult.then(userData => {
+        if (needSetCookie) {
+          res.setHeader("Set-Cookie", `userid=${userId};path=/; httpOnly;expires=${getCookieExpires()}`)
+        }
         res.end(JSON.stringify(userData));
       })
       return;
--- blog-1/src/router/user.js
+++ blog-1/src/router/user.js
@@ -3,35 +3,41 @@
  * @Author: Looper
  * @Date: 2020-05-31 21:14:03
  * @LastEditors: Looper
- * @LastEditTime: 2020-06-06 23:53:50
+ * @LastEditTime: 2020-06-07 11:49:19
  * @FilePath: /nodejs/blog-1/src/router/user.js
  * @Url: 
  */
-const { loginCheck } = require("../controller/user")
+const { login } = require("../controller/user")
 const { SuccessModel, ErrorModel } = require("../model/resModel")
-
-// 获取cookie的过期时间
-const getCookieExpires = () => {
-  const d = new Date();
-  d.setTime(d.getTime() + (24 * 60 * 60 * 1000));
-  console.log('d.toGMTString() is ', d.toGMTString());
-  return d.toGMTString();
-}
 
 const handleUserRouter = (req, res) => {
   const method = req.method;
   const path = req.path;
 
-  if (method === "POST" && path === "/api/user/login") {
-    const { username, password } = req.body;
-    const result = loginCheck(username, password);
+  if (method === "GET" && path === "/api/user/login") {
+    const { username, password } = req.query;
+    const result = login(username, password);
     return result.then(data => {
       if (data.username) {
-        res.setHeader("Set-Cookie", `username=${data.username}; path=/; httpOnly; expires=${getCookieExpires()}`)
+        req.session.username = data.username;
+        req.session.realName = data.realname;
+
+        console.log("req.session is ", req.session);
         return new SuccessModel("登录成功");
       }
       return new ErrorModel("登录失败");
     })
+  }
+
+  if (method === "GET" && req.path === "/api/user/login-test") {
+    if (req.session.username) {
+      return Promise.resolve(
+        new SuccessModel({
+          session: req.session
+        })
+      )
+    }
+    return Promise.resolve(new ErrorModel("尚未登录！"));
   }
 }
 
